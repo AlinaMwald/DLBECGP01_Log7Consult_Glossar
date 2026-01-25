cbc264cc5d88f4b3bf3884fb1f5cb6b0e25e5234
Alina Mirwald <68157658+AlinaMwald@users.noreply.github.com>
2026-01-25 22:06:20 +0100

    Add Search / not working

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,26 @@
+/* Suchfunktion Glossar Übericht nach Buchstaben */
+
+function myFunction() {
+  // Benötigte Variablen deklarieren
+  var input, filter, letters, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  letters = document.querySelectorAll('.letter');
+
+  // Loop through all letter spans, and hide those who don't match the search query
+  for (i = 0; i < letters.length; i++) {
+    txtValue = letters[i].textContent || letters[i].innerText;
+    var colDiv = letters[i].parentElement; // div.col-12
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      colDiv.style.display = "";
+    } else {
+      colDiv.style.display = "none";
+    }
+  }
+}
+
+
+
 // GLOSSAR-EINTRÄGE AUTOMATISIERT AUS JSON LADEN FÜR GLOSSAR-STARTSEITE TEST
 /*
 fetch("json/data.json")
